bf7cb3d666ee89e92461f46ad55a68a61ef9705f
meddeb23 <dalimeddeb132@gmail.com>
2022-08-12 21:46:00 +0100

    format midnight time

--- server/utilities/format-time.js
+++ server/utilities/format-time.js
@@ -10,6 +10,7 @@ function stringToMinutes(timeString) {
   return hours * 60 + minutes;
 }
 function minutesToString(minutes) {
+  minutes = minutes % 1440;
   const hours = ("0" + parseInt(minutes / 60)).slice(-2);
   const min = ("0" + (minutes % 60)).slice(-2);
   return hours + ":" + min;
